736b8cc8d6d43b054a2d5debc8621bd4b83468de
Sameer Singh <sameeersingh@gmail.com>
2015-11-19 19:48:34 -0800

    personal: support for jekyll so that bibere output directly goes to site

--- bibere-core/src/main/scala/org/sameersingh/bibere/Personal.scala
+++ bibere-core/src/main/scala/org/sameersingh/bibere/Personal.scala
@@ -1,5 +1,7 @@
 package org.sameersingh.bibere
 
+import java.io.PrintWriter
+
 /**
  * @author sameer
  */
@@ -9,6 +11,25 @@ object Personal {
 
   val htmlWriter = new BootstrapWriter with PerYear {
     override def mainAuthorId = Some("sameer")
+
+    override def pre(writer: PrintWriter): Unit = {
+      writer.println(
+        """
+          |---
+          |layout: main
+          |title: "Sameer Singh: Publications"
+          |#permalink: /pubs
+          |---
+          |<h2>Publications</h2>
+          |
+          |<div>
+          |  Publications (grouped by year): <a href="#2016">2016</a>, <a href="#2015">2015</a>, <a href="#2014">2014</a>, <a href="#2013">2013</a>, <a href="#2012">2012</a>, <a href="#2011">2011</a>, <a href="#2010">2010</a>, <a href="#2009">2009</a>, <a href="#2008">2008</a>, <a href="#2007">2007</a>, <a href="#2006">&lt;=2006</a>.
+          |  <hr>
+          |</div>
+        """.stripMargin.trim)
+    }
+
+    override def post(writer: PrintWriter): Unit = {}
   }
 
   val bibtexWriter = new BibtexWriter {
